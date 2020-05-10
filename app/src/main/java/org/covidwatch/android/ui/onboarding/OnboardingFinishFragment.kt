5ec6aa597253f545634ecbae395e538766d1b23d
madim <murzabek@kolesa.kz>
2020-05-11 00:00:08 +0600

    Enable Exposure Notifications

--- app/src/main/java/org/covidwatch/android/ui/onboarding/OnboardingFinishFragment.kt
+++ app/src/main/java/org/covidwatch/android/ui/onboarding/OnboardingFinishFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import org.covidwatch.android.R
 import org.covidwatch.android.databinding.FragmentOnboardingFinishBinding
+import org.covidwatch.android.exposurenotification.ENStatus
 import org.covidwatch.android.ui.BaseFragment
+import org.covidwatch.android.ui.event.EventObserver
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class OnboardingFinishFragment : BaseFragment<FragmentOnboardingFinishBinding>() {
+
+    private val onboardingFinishViewModel: OnboardingFinishViewModel by viewModel()
 
     override fun binding(
         inflater: LayoutInflater,
@@ -20,12 +26,25 @@ class OnboardingFinishFragment : BaseFragment<FragmentOnboardingFinishBinding>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.enableButton.setOnClickListener {
-            // TODO: enable exposure-notifications
-            findNavController().popBackStack(R.id.homeFragment, false)
+            onboardingFinishViewModel.onEnableClicked()
         }
 
         binding.notNowButton.setOnClickListener {
             findNavController().popBackStack(R.id.homeFragment, false)
         }
+
+        onboardingFinishViewModel.exposureNotificationResult.observe(viewLifecycleOwner, EventObserver {
+            val enStatus = it.left
+            if (enStatus == ENStatus.SUCCESS) {
+                findNavController().popBackStack(R.id.homeFragment, false)
+                return@EventObserver
+            }
+
+            showError(enStatus)
+        })
+    }
+
+    private fun showError(enStatus: ENStatus?) {
+        Snackbar.make(binding.root, R.string.generic_error_message, Snackbar.LENGTH_LONG).show()
     }
 }
