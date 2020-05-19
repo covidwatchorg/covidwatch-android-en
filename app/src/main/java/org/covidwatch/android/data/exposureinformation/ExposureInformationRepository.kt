ccaaf6e2c8fb3ebb24b79119db45774f03928a15
Susan Crayne <susancrayne@yahoo.com>
2020-05-19 11:21:24 -0400

    moved the rest of the logic to ExposureInformationRepository.kt

--- app/src/main/java/org/covidwatch/android/data/exposureinformation/ExposureInformationRepository.kt
+++ app/src/main/java/org/covidwatch/android/data/exposureinformation/ExposureInformationRepository.kt
@@ -1,19 +1,16 @@
 package org.covidwatch.android.data.exposureinformation
 
-import android.app.Activity
 import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.fragment.findNavController
+import com.google.android.gms.nearby.exposurenotification.ExposureSummary
 import kotlinx.coroutines.GlobalScope
 import org.covidwatch.android.R
 import org.covidwatch.android.data.CovidExposureInformation
 import org.covidwatch.android.data.toCovidExposureInformation
 import org.covidwatch.android.exposurenotification.RandomEnObjects
-import org.covidwatch.android.exposurenotification.TestExposureNotification
 import org.covidwatch.android.extension.io
-import org.covidwatch.android.extension.mutableLiveData
-import androidx.navigation.fragment.findNavController
+import org.covidwatch.android.data.CovidExposureSummary
+import org.covidwatch.android.data.pref.SharedPreferenceStorage
 
 
 class ExposureInformationRepository(private val local: ExposureInformationLocalSource) {
@@ -40,7 +37,6 @@ class ExposureInformationRepository(private val local: ExposureInformationLocalS
 
     fun addFakeItem(context: Context){
         var returnExposureInformationList : List<CovidExposureInformation>
-        val testExposureNotification = TestExposureNotification()
         val covidExposureInformation: CovidExposureInformation =
             RandomEnObjects.exposureInformation.toCovidExposureInformation()
         var exposureInformationList: List<CovidExposureInformation> =
@@ -48,8 +44,8 @@ class ExposureInformationRepository(private val local: ExposureInformationLocalS
 
         GlobalScope.io {
             returnExposureInformationList = saveOneGetAll(exposureInformationList)
-            //sum up risk exposures from returnExposureInformationList and pass to TestExposureNotification
-            testExposureNotification.saveExposureSummaryInPreferences(context,covidExposureInformation,returnExposureInformationList.size)
+            //sum up risk exposures from returnExposureInformationList and pass to saveExposureSummaryInPreferences
+            saveExposureSummaryInPreferences(context,covidExposureInformation,returnExposureInformationList.size)
         }
     }
 
@@ -62,6 +58,31 @@ class ExposureInformationRepository(private val local: ExposureInformationLocalS
         var newExposureInformationList: List<CovidExposureInformation>
         newExposureInformationList = randomExposureInformation()
         return newExposureInformationList
+    }
+
+    fun saveExposureSummaryInPreferences(
+        context: Context,
+        covidExposureInformation: CovidExposureInformation,
+        matchedKeyCount: Int)
+    {
+        var sharedPreferences: SharedPreferenceStorage = SharedPreferenceStorage(context)
+        val exposureSummaryRandom: ExposureSummary = RandomEnObjects.exposureSummary
+        RandomEnObjects.retrieved = true
+        val daysSinceLastExposure: Int = exposureSummaryRandom.daysSinceLastExposure
+        val maximumRiskScore = exposureSummaryRandom.maximumRiskScore
+        val attenuationDurations: IntArray = intArrayOf(1)
+        attenuationDurations[0] = covidExposureInformation.attenuationValue
+        val summationRiskScore = covidExposureInformation.totalRiskScore
+        var covidExposureSummary: CovidExposureSummary = CovidExposureSummary(
+            daysSinceLastExposure, matchedKeyCount, maximumRiskScore, attenuationDurations, summationRiskScore )
+        val prefs = context.applicationContext.getSharedPreferences(
+            "exposure_summary",
+            Context.MODE_PRIVATE
+        )
+        with (prefs.edit()) {
+            sharedPreferences.exposureSummary = covidExposureSummary
+            commit()
+        }
     }
 
 }
--- app/src/main/java/org/covidwatch/android/exposurenotification/TestExposureNotification.kt
+++ app/src/main/java/org/covidwatch/android/exposurenotification/TestExposureNotification.kt
@@ -34,6 +34,5 @@ class TestExposureNotification {
             sharedPreferences.exposureSummary = covidExposureSummary
             commit()
         }
-        val exposureSummary: CovidExposureSummary = sharedPreferences.exposureSummary
     }
 }
