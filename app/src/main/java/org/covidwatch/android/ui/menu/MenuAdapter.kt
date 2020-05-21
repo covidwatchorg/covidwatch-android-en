1c71e08dcb53369fcc7c342ecef9e43aa5b530d6
Susan Crayne <susancrayne@yahoo.com>
2020-05-21 11:32:38 -0400

    first changes for Menu screen, Version 2

--- app/src/main/java/org/covidwatch/android/ui/menu/MenuAdapter.kt
+++ app/src/main/java/org/covidwatch/android/ui/menu/MenuAdapter.kt
@@ -10,13 +10,17 @@ class MenuAdapter(
 ) : RecyclerView.Adapter<MenuItemViewHolder>() {
 
     private val items = listOf(
+        MenuItem(R.string.generate_random_exposure, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
         MenuItem(R.string.settings, 0, Settings),
-        MenuItem(R.string.test_results, 0, TestResults),
-        MenuItem(R.string.how_does_this_work, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.covid_watch_website, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
+        MenuItem(R.string.possible_exposures, 0, TestResults),
+        MenuItem(R.string.notify_others, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
+        MenuItem(R.string.how_the_app_works, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
         MenuItem(R.string.health_guidelines, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
+        MenuItem(R.string.covid_watch_website, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
+        MenuItem(R.string.faq, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
         MenuItem(R.string.terms_of_use, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.privacy_policy, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/"))
+        MenuItem(R.string.privacy_policy, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
+        MenuItem(R.string.get_support, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/"))
     )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuItemViewHolder {
