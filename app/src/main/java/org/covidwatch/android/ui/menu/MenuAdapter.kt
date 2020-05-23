7ed309093518607b3add31069995fc3fa865f217
Susan Crayne <susancrayne@yahoo.com>
2020-05-22 20:46:58 -0400

    made integer resource for maximum safe risk score

--- app/src/main/java/org/covidwatch/android/ui/menu/MenuAdapter.kt
+++ app/src/main/java/org/covidwatch/android/ui/menu/MenuAdapter.kt
@@ -5,8 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.covidwatch.android.R
-import org.covidwatch.android.data.CovidExposureSummary
-import org.covidwatch.android.data.pref.get
+import org.covidwatch.android.data.pref.SharedPreferenceStorage
 
 class MenuAdapter(
     private val onClick: ((destination: Destination) -> Unit)
@@ -15,17 +14,58 @@ class MenuAdapter(
     private var isHighRisk: Boolean = false
 
     private val items = listOf(
-        MenuItem(R.string.generate_random_exposure, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.settings, 0, Settings),
-        MenuItem(R.string.possible_exposures, R.drawable.ic_info_red, TestResults),
-        MenuItem(R.string.notify_others, 0, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.how_the_app_works, 0, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.health_guidelines, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.covid_watch_website, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.faq, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.terms_of_use, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.privacy_policy, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/")),
-        MenuItem(R.string.get_support, R.drawable.ic_exit_to_app, Browser("https://www.covid-watch.org/"))
+        MenuItem(
+            R.string.generate_random_exposure,
+            R.drawable.ic_exit_to_app,
+            Browser("https://www.covid-watch.org/")
+        ),
+        MenuItem(
+            R.string.settings,
+            0,
+            Settings
+        ),
+        MenuItem(
+            R.string.possible_exposures,
+            R.drawable.ic_info_red,
+            TestResults),
+        MenuItem(
+            R.string.notify_others,
+            0,
+            Browser("https://www.covid-watch.org/")),
+        MenuItem(
+            R.string.how_the_app_works,
+            0,
+            Browser("https://www.covid-watch.org/")),
+        MenuItem(
+            R.string.health_guidelines,
+            R.drawable.ic_exit_to_app,
+            Browser("https://www.covid-watch.org/")
+        ),
+        MenuItem(
+            R.string.covid_watch_website,
+            R.drawable.ic_exit_to_app,
+            Browser("https://www.covid-watch.org/")
+        ),
+        MenuItem(
+            R.string.faq,
+            R.drawable.ic_exit_to_app,
+            Browser("https://www.covid-watch.org/")
+        ),
+        MenuItem(
+            R.string.terms_of_use,
+            R.drawable.ic_exit_to_app,
+            Browser("https://www.covid-watch.org/")
+        ),
+        MenuItem(
+            R.string.privacy_policy,
+            R.drawable.ic_exit_to_app,
+            Browser("https://www.covid-watch.org/")
+        ),
+        MenuItem(
+            R.string.get_support,
+            R.drawable.ic_exit_to_app,
+            Browser("https://www.covid-watch.org/")
+        )
     )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuItemViewHolder {
@@ -38,7 +78,7 @@ class MenuAdapter(
 
     override fun onBindViewHolder(holder: MenuItemViewHolder, position: Int) {
         var menuItem = items[position]
-        if (menuItem.title == R.string.possible_exposures){
+        if (menuItem.title == R.string.possible_exposures) {
             menuItem = updateMenuItemForRisk(menuItem)
         }
 
@@ -50,22 +90,20 @@ class MenuAdapter(
 
     }
 
-    //TODO get actual high risk
     private fun updateMenuItemForRisk(menuItem: MenuItem): MenuItem {
-        if (isHighRisk){
+        if (isHighRisk) {
             menuItem.iconEnd = R.drawable.ic_info_red
-        }
-        else {
+        } else {
             menuItem.iconEnd = 0
         }
         return menuItem
     }
 
-    private fun getIsHighRisk (context: Context): Boolean{
-        val prefs = context.getSharedPreferences("org.covidwatch.android.PREFERENCE_FILE_KEY",Context.MODE_PRIVATE)
-        val exposureSummary: CovidExposureSummary = prefs.get("exposureSummary", null) as CovidExposureSummary
-        val maximumRiskScore = exposureSummary.maximumRiskScore
-        return maximumRiskScore > 6
+    private fun getIsHighRisk(context: Context): Boolean {
+        val prefs = SharedPreferenceStorage(context)
+        var userMaximumRiskScore = prefs.exposureSummary.maximumRiskScore
+        var safeMaximumRiskScore = context.getResources().getInteger(R.integer.maximum_risk_score)
+        return userMaximumRiskScore > safeMaximumRiskScore
     }
 
 }
