edcc91afdd05949a35a03733b2772bcc1e0ce401
Apisov <apisovp@gmail.com>
2020-05-31 20:49:34 +0200

    Add EN permissions handling

--- app/src/main/java/org/covidwatch/android/ui/BaseViewModel.kt
+++ app/src/main/java/org/covidwatch/android/ui/BaseViewModel.kt
@@ -1,13 +1,16 @@
 package org.covidwatch.android.ui
 
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.app.Activity
+import android.util.SparseArray
+import androidx.core.util.contains
+import androidx.lifecycle.*
+import com.google.android.gms.common.api.ApiException
 import org.covidwatch.android.domain.LiveDataUseCase
 import org.covidwatch.android.exposurenotification.ENStatus
 import org.covidwatch.android.extension.observeUseCase
+import org.covidwatch.android.extension.send
 import org.covidwatch.android.functional.Either
+import org.covidwatch.android.ui.event.Event
 
 /**
  * Base ViewModel class with default ENStatus handling.
@@ -18,6 +21,31 @@ abstract class BaseViewModel : ViewModel() {
     private val _status = MediatorLiveData<ENStatus>()
     val status: MutableLiveData<ENStatus> = _status
 
+    private val _resolvable = MutableLiveData<Event<Resolvable>>()
+    val resolvable: LiveData<Event<Resolvable>> = _resolvable
+
+    private val tasksInResolution = SparseArray<suspend () -> Any>()
+
+    private fun handleStatus(status: ENStatus) {
+        _status.value = status
+    }
+
+    protected suspend fun <V> withPermission(
+        requestCode: Int,
+        task: suspend () -> Either<ENStatus, V>
+    ) {
+        if (!tasksInResolution.contains(requestCode)) {
+            task().apply {
+                failure {
+                    if (it is ENStatus.NeedsResolution) {
+                        _resolvable.send(Resolvable(it.exception, requestCode))
+                        tasksInResolution.put(requestCode, task)
+                    }
+                }
+            }
+        }
+    }
+
     protected fun <T, P> observeStatus(
         useCase: LiveDataUseCase<T, P>,
         params: P? = null,
@@ -25,9 +53,26 @@ abstract class BaseViewModel : ViewModel() {
     ) {
         val liveData = viewModelScope.observeUseCase(useCase, params)
         _status.addSource(liveData) {
-            it.failure { status -> _status.value = status }
+            it.failure(this::handleStatus)
             block(it)
             _status.removeSource(liveData)
         }
     }
+
+    suspend fun handleResolution(requestCode: Int, resultCode: Int) {
+        val task = tasksInResolution.get(requestCode)
+        tasksInResolution.remove(requestCode)
+        if (resultCode == Activity.RESULT_OK) {
+            val result = task() as Either<*, *>
+            val status = result.left as? ENStatus
+            status?.let { handleStatus(it) }
+        } else {
+            handleStatus(ENStatus.FailedRejectedOptIn)
+        }
+    }
+
+    data class Resolvable(
+        val apiException: ApiException,
+        val requestCode: Int
+    )
 }
