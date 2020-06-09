858c52b50194a8823a7d9c7e755d412b762f0b76
Apisov <apisovp@gmail.com>
2020-06-09 18:36:50 +0200

    Use RiskScore in the domain models

--- app/src/main/java/org/covidwatch/android/data/CovidExposureSummary.kt
+++ app/src/main/java/org/covidwatch/android/data/CovidExposureSummary.kt
@@ -1,10 +1,21 @@
 package org.covidwatch.android.data
 
+import com.google.android.gms.nearby.exposurenotification.ExposureSummary
+
 class CovidExposureSummary(
     val daySinceLastExposure: Int,
     val matchedKeyCount: Int,
-    val maximumRiskScore: Int,
+    val maximumRiskScore: RiskScore,
     val attenuationDurationsInMinutes: IntArray,
-    val summationRiskScore: Int,
+    // TODO: 09.06.2020 Check if summation risk score complies the same logic as all risk scores
+    val summationRiskScore: RiskScore,
     val modifiedTime: Long = System.currentTimeMillis()
+)
+
+fun ExposureSummary.asCovidExposureSummary() = CovidExposureSummary(
+    daysSinceLastExposure,
+    matchedKeyCount,
+    maximumRiskScore,
+    attenuationDurationsInMinutes,
+    summationRiskScore
 )
