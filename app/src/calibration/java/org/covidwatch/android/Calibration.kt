4b7b3a3a8d24cb984358ffeba64847030812010e
Apisov <apisovp@gmail.com>
2020-06-18 17:41:24 +0200

    Add first threshold [30, 33] to the list of the test thresholds

--- app/src/calibration/java/org/covidwatch/android/Calibration.kt
+++ app/src/calibration/java/org/covidwatch/android/Calibration.kt
@@ -2,13 +2,14 @@ package org.covidwatch.android
 
 val attenuationDurationThresholds: List<IntArray>
     get() {
-        var start = intArrayOf(30, 33)
-        val tmp = mutableListOf<IntArray>()
-        intArrayOf(30, 33)
+        var threshold = intArrayOf(30, 33)
+        val thresholds = mutableListOf(threshold)
+
         for (i in 0..66 step 3) {
-            start = start.map { it + 3 }.toIntArray()
-            tmp.add(start)
+            threshold = threshold.map { it + 3 }.toIntArray()
+
+            thresholds.add(threshold)
         }
-        return tmp
+        return thresholds
     }
 
