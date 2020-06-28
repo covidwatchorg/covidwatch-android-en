58efcf175f8f0d2aaf1e267d2c92df3cee299c9a
Apisov <apisovp@gmail.com>
2020-06-28 17:19:19 +0200

    Fix attenuation thresholds generation

--- app/src/calibration/java/org/covidwatch/android/Calibration.kt
+++ app/src/calibration/java/org/covidwatch/android/Calibration.kt
@@ -2,14 +2,7 @@ package org.covidwatch.android
 
 val attenuationDurationThresholds: List<IntArray>
     get() {
-        var threshold = intArrayOf(30, 33)
-        val thresholds = mutableListOf(threshold)
-
-        for (i in 0..66 step 3) {
-            threshold = threshold.map { it + 3 }.toIntArray()
-
-            thresholds.add(threshold)
-        }
-        return thresholds
+        val startThreshold = intArrayOf(30, 33)
+        return (0..66 step 3).map { i -> startThreshold.map { it + i }.toIntArray() }
     }
 
