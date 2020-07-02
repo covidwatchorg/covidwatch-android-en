5c5a52975c793d0b1859ab3ddad987616e3e55f7
Ápisov <Apisov@users.noreply.github.com>
2020-07-02 20:34:31 +0200

    Fix/process all exposure state updates (#103)
    * Run UpdateExposureStateWork sequentially without canceling previous works
    
    * Move providing keys from a file to background service

--- app/src/calibration/java/org/covidwatch/android/ui/MainActivity.kt
+++ app/src/calibration/java/org/covidwatch/android/ui/MainActivity.kt
@@ -5,16 +5,16 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
 import androidx.lifecycle.lifecycleScope
+import androidx.work.WorkManager
 import com.jaredrummler.android.device.DeviceName
 import org.covidwatch.android.domain.ProvideDiagnosisKeysFromFileUseCase
 import org.covidwatch.android.domain.ProvideDiagnosisKeysFromFileUseCase.Params
 import org.covidwatch.android.extension.launchUseCase
+import org.covidwatch.android.work.ProvideDiagnosisKeysFromFileWork
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 
 class MainActivity : BaseMainActivity() {
-
-    private val provideDiagnosisKeysFromFileUseCase: ProvideDiagnosisKeysFromFileUseCase by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +31,7 @@ class MainActivity : BaseMainActivity() {
         if (intent?.action == Intent.ACTION_SEND && "application/zip" == intent.type) {
             (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
                 Timber.d("Received a test diagnosis file: ${it.path}")
-                lifecycleScope.launchUseCase(provideDiagnosisKeysFromFileUseCase, Params(it))
+                ProvideDiagnosisKeysFromFileWork.start(it, WorkManager.getInstance(this))
             }
         }
     }
