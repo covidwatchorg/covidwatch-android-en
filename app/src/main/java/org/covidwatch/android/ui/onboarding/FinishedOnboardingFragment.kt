a153d1d72aff3c7aab9162c3c047fab6617dfe74
Apisov <apisovp@gmail.com>
2020-07-12 17:31:07 +0200

    Change onboarding text

--- app/src/main/java/org/covidwatch/android/ui/onboarding/FinishedOnboardingFragment.kt
+++ app/src/main/java/org/covidwatch/android/ui/onboarding/FinishedOnboardingFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import org.covidwatch.android.R
 import org.covidwatch.android.databinding.FragmentFinishedOnboardingBinding
-import org.covidwatch.android.extension.shareApp
 import org.covidwatch.android.ui.BaseFragment
 
 class FinishedOnboardingFragment : BaseFragment<FragmentFinishedOnboardingBinding>() {
@@ -20,9 +19,6 @@ class FinishedOnboardingFragment : BaseFragment<FragmentFinishedOnboardingBindin
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.shareAppButton.setOnClickListener {
-            context?.shareApp()
-        }
 
         binding.seeExposureSummary.setOnClickListener {
             findNavController().popBackStack(R.id.homeFragment, false)
