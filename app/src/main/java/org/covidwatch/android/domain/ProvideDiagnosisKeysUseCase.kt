b482cd7927436caae33191a9364515f3759fdbbe
Apisov <apisovp@gmail.com>
2020-07-27 22:41:50 +0200

    Fetch keys every 6 hours instead of 24

--- app/src/main/java/org/covidwatch/android/domain/ProvideDiagnosisKeysUseCase.kt
+++ app/src/main/java/org/covidwatch/android/domain/ProvideDiagnosisKeysUseCase.kt
@@ -64,7 +64,7 @@ class ProvideDiagnosisKeysUseCase(
     data class Params(val recurrent: Boolean)
 
     companion object {
-        private const val RECURRENCE_PERIOD = 24L // Hours
+        private const val RECURRENCE_PERIOD = 6L // Hours
         const val WORK_NAME = "provide_diagnosis_keys"
         const val RECURRENT_WORK_NAME = "provide_diagnosis_keys_daily"
     }
