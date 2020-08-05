e28c85b22554138de6a3727697901c4ac2524ddd
Apisov <apisovp@gmail.com>
2020-08-05 12:39:18 +0200

    Change Arizona logo

--- app/src/main/java/org/covidwatch/android/ui/RegionResources.kt
+++ app/src/main/java/org/covidwatch/android/ui/RegionResources.kt
@@ -9,7 +9,7 @@ import org.covidwatch.android.data.RegionId.*
 val Region.bigLogo
     @DrawableRes
     get() = when (id) {
-        ARIZONA_STATE -> R.drawable.arizona_big_logo
+        ARIZONA_STATE -> R.drawable.az_big_logo
         UOA -> R.drawable.uoa_big_logo
         ASU -> R.drawable.asu_big_logo
         NAU -> R.drawable.nau_big_logo
@@ -19,7 +19,7 @@ val Region.bigLogo
 val Region.logo
     @DrawableRes
     get() = when (id) {
-        ARIZONA_STATE -> R.drawable.arizona_logo
+        ARIZONA_STATE -> R.drawable.az_logo
         UOA -> R.drawable.uoa_logo
         ASU -> R.drawable.asu_logo
         NAU -> R.drawable.nau_logo
