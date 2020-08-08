b8ec75dc3592344cd084999ed85f5aaa09feefe2
Ápisov <Apisov@users.noreply.github.com>
2020-08-08 18:12:24 +0200

    Feature/forced select region (#134)
    * Removed unused SelectedRegionPreviewFragment.kt
    
    * Add ResourcesProvider.kt
    
    * Force regions selection during onboarding
    
    * Fix typo in full app name

--- app/src/main/java/org/covidwatch/android/ui/selectregion/SelectRegionViewModel.kt
+++ app/src/main/java/org/covidwatch/android/ui/selectregion/SelectRegionViewModel.kt
@@ -4,26 +4,40 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
+import org.covidwatch.android.R
 import org.covidwatch.android.data.pref.PreferenceStorage
 import org.covidwatch.android.extension.send
+import org.covidwatch.android.ui.IResourcesProvider
 import org.covidwatch.android.ui.event.Event
 
-class SelectRegionViewModel(private val preferences: PreferenceStorage) : ViewModel() {
+class SelectRegionViewModel(
+    private val preferences: PreferenceStorage,
+    private val resources: IResourcesProvider
+) : ViewModel() {
     private var onboarding: Boolean = false
 
     private val _closeScreen = MutableLiveData<Event<Unit>>()
     val closeScreen: LiveData<Event<Unit>> = _closeScreen
 
-    private val _showRegionPreviewScreen = MutableLiveData<Event<Unit>>()
-    val showRegionPreviewScreen: LiveData<Event<Unit>> = _showRegionPreviewScreen
+    private val _showSetupCompleteScreen = MutableLiveData<Event<Unit>>()
+    val showSetupCompleteScreen: LiveData<Event<Unit>> = _showSetupCompleteScreen
+
+    private val _showContinueButton = MutableLiveData<Boolean>()
+    val showContinueButton: LiveData<Boolean> = _showContinueButton
 
     fun continueClicked() {
-        if (onboarding) _showRegionPreviewScreen.send()
+        if (onboarding) _showSetupCompleteScreen.send()
         else _closeScreen.send()
     }
 
     fun selectedRegion(position: Int) {
-        preferences.selectedRegion = position
+        if (onboarding) {
+            // Show continue if not first region selected which is a dummy "select region" item
+            _showContinueButton.value = position != 0
+            if (position > 0) preferences.selectedRegion = position - 1
+        } else {
+            preferences.selectedRegion = position
+        }
     }
 
     fun setOnboarding(boolean: Boolean?) {
@@ -34,6 +48,13 @@ class SelectRegionViewModel(private val preferences: PreferenceStorage) : ViewMo
         get() = preferences.selectedRegion
 
     val regions = preferences.observableRegions.map { regions ->
-        regions.regions.map { it.name }
+        if (onboarding) {
+            mutableListOf<String>().apply {
+                add(resources.getString(R.string.select_region_label))
+                addAll(regions.regions.map { it.name })
+            }
+        } else {
+            regions.regions.map { it.name }
+        }
     }
 }
