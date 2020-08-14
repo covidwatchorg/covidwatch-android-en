82c4dd46d5640ecf83d7a575231d2cc7e1538e92
Apisov <apisovp@gmail.com>
2020-08-15 01:43:51 +0200

    Remove Firebase from production build

--- app/src/production/java/org/covidwatch/android/CovidWatchApplication.kt
+++ app/src/production/java/org/covidwatch/android/CovidWatchApplication.kt
@@ -1,10 +1,3 @@
 package org.covidwatch.android
 
-import timber.log.Timber
-
-class CovidWatchApplication : BaseCovidWatchApplication() {
-    override fun onCreate() {
-        super.onCreate()
-        Timber.plant(CrashlyticsTree())
-    }
-}
+class CovidWatchApplication : BaseCovidWatchApplication()
