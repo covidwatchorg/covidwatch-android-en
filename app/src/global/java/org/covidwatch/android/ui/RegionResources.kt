699e54afedc018685ab20a3b8f0b727a004264e4
Ápisov <Apisov@users.noreply.github.com>
2020-08-27 22:55:39 +0200

    Feature/bermuda build (#144)
    * Add global build flavor
    
    * Add Bermuda logo
    
    * Separate global and arizona region resources logic
    
    * Separate global and arizona region resources
    
    * Change regions JSON for the global app flavor
    
    * Change Bermuda id to 4
    
    * Use Covid Watch Bermuda Exposure Notifications App
    
    * Use "Covid Watch Exposure Notifications App" in the global version of the app
    
    * Change privacy policy link for the global build
    
    * Add new Firebase json files to CI
    
    * Fi CI yml formatting
    
    * Fix CI yml
    
    * Fix CI yml?
    
    * Use proper Firebase JSON for Arizona
    
    * Create src/calibrationArizona in CI
    
    * Increase Gradle memory size
    
    * Update SonarCube project key

---
+++ app/src/global/java/org/covidwatch/android/ui/RegionResources.kt
@@ -0,0 +1,39 @@
+package org.covidwatch.android.ui
+
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import org.covidwatch.android.R
+import org.covidwatch.android.data.Region
+import org.covidwatch.android.data.RegionId.BERMUDA
+
+val Region.bigLogo
+    @DrawableRes
+    get() = when (id) {
+        BERMUDA -> R.drawable.bermuda_logo
+        else -> R.drawable.cw_big_logo
+    }
+
+val Region.logo
+    @DrawableRes
+    get() = when (id) {
+        BERMUDA -> R.drawable.bermuda_logo
+        else -> R.drawable.cw_logo
+    }
+
+val Region.logoDescription
+    @StringRes
+    get() = when (id) {
+        BERMUDA -> R.string.az_logo_content_description
+        else -> R.string.generic_logo_content_description
+    }
+
+fun ImageView.setBigLogo(region: Region) {
+    setImageResource(region.bigLogo)
+    contentDescription = context.getString(region.logoDescription)
+}
+
+fun ImageView.setLogo(region: Region) {
+    setImageResource(region.logo)
+    contentDescription = context.getString(region.logoDescription)
+}
---
+++ app/src/global/java/org/covidwatch/android/ui/Urls.kt
@@ -0,0 +1,9 @@
+package org.covidwatch.android.ui
+
+object Urls {
+    const val HEALTH_GUIDELINES = "https://www.cdc.gov/coronavirus/2019-ncov/index.html"
+    const val COVIDWATCH_WEBSITE = "https://www.covidwatch.org"
+    const val PRIVACY = "https://www.covidwatch.org/privacy-policy"
+    const val SUPPORT = "https://www.covidwatch.org/get_support"
+    const val FAQ = "https://www.covidwatch.org/faq"
+}
