bb58badca813664cc79759f2ee2c1ea6b5167d12
Ápisov <Apisov@users.noreply.github.com>
2020-08-31 19:51:35 +0200

    Add separate implementations of CountryCodeRepository for global and arizona apps (#147)

---
+++ app/src/arizona/java/org/covidwatch/android/data/countrycode/DefaultCountryCodeRepository.kt
@@ -0,0 +1,26 @@
+package org.covidwatch.android.data.countrycode
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.withContext
+import org.covidwatch.android.data.CountryCode
+import org.covidwatch.android.domain.AppCoroutineDispatchers
+import org.covidwatch.android.extension.io
+
+class DefaultCountryCodeRepository(
+    private val local: CountryCodeDao,
+    private val dispatchers: AppCoroutineDispatchers
+) : CountryCodeRepository {
+
+    //TODO: Remove when we have actual logic for tracking current and visited countries
+    init {
+        GlobalScope.io {
+            if (local.countryCodes().isEmpty()) {
+                local.insert(CountryCode("US"))
+            }
+        }
+    }
+
+    override suspend fun exposureRelevantCountryCodes() = withContext(dispatchers.io) {
+        local.countryCodes().map { it.code }
+    }
+}
